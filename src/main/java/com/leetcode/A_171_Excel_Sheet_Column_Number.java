67a0155156706d4cfec0cc4a635454c18e3f8ccf
marklen <marklen86@gmail.com>
2022-10-11 20:44:04 -0500

    A_171_Excel_Sheet_Column_Number

--- src/main/java/com/leetcode/A_171_Excel_Sheet_Column_Number.java
+++ src/main/java/com/leetcode/A_171_Excel_Sheet_Column_Number.java
@@ -3,6 +3,9 @@ package com.leetcode;
 public class A_171_Excel_Sheet_Column_Number {
     public static void main(String[] args) {
         System.out.println(  titleToNumber("AAA")  );
+
+        // AAA->703
+
     }
 
 
@@ -13,6 +16,10 @@ public class A_171_Excel_Sheet_Column_Number {
             result = result * 26 + (columnTitle.toUpperCase().charAt(i) - 64);
         }
         return result;
+
+        // i=0  0*26+'A'(65)-64=1
+        // i=1  1*26+'A'(65)-64=27
+        // i=2  27*26+'A'(65)-64=703
     }
 
 }
