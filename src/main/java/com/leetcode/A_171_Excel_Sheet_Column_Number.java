d88190a29c6c1b0ae675c412db92338b455b804f
marklen <marklen86@gmail.com>
2022-10-19 20:30:23 -0500

    add some comments

--- src/main/java/com/leetcode/A_171_Excel_Sheet_Column_Number.java
+++ src/main/java/com/leetcode/A_171_Excel_Sheet_Column_Number.java
@@ -3,7 +3,6 @@ package com.leetcode;
 public class A_171_Excel_Sheet_Column_Number {
     public static void main(String[] args) {
         System.out.println(  titleToNumber("AAA")  );
-
         // AAA->703
 
     }
@@ -16,10 +15,18 @@ public class A_171_Excel_Sheet_Column_Number {
             result = result * 26 + (columnTitle.toUpperCase().charAt(i) - 64);
         }
         return result;
-
+        //"AAA"->703
         // i=0  0*26+'A'(65)-64=1
         // i=1  1*26+'A'(65)-64=27
         // i=2  27*26+'A'(65)-64=703
+
+
+//        int result = 0;
+//        int p= columnTitle.length()-1;
+//        for (char c:columnTitle.toCharArray()) {
+//            result+=(c-'A'+1) * Math.pow(26,p--);
+//        }
+//        return result;  O(n)
     }
 
 }
