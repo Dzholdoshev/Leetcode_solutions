e117929682f0635a188700d6d3c7dea15fd09507
marklen <marklen86@gmail.com>
2022-11-19 13:19:37 -0600

    Linked List Cycle

---
+++ src/main/java/com/leetcode/A_142_Linked_List_Cycle_II.java
@@ -0,0 +1,23 @@
+package com.leetcode;
+
+public class A_142_Linked_List_Cycle_II {
+
+    static ListNode detectCycleWithTwoPointer(ListNode head) {
+        if (head == null || head.next == null) return null;
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                ListNode firstNode = head;
+                ListNode intersection = fast;
+                while (firstNode != intersection) {
+                    intersection = intersection.next;
+                    firstNode = firstNode.next;
+                }
+                return firstNode;
+            }
+        }
+        return null;
+    }
+}
